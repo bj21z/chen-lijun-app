7159f982e1fc9e11aec761aba7b32b9255c0b022
bj21z <bj21z@sina.com>
2026-08-05 20:00:39 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chen-lijun-v1.0.0';
+const CACHE='chen-lijun-1.1.0';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/dynamics.json','./assets/icon.svg','./assets/images/longmen.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
